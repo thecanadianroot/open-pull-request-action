8e667692d58d95f9336a4469b0c355ae446e27ef
Guillaume Tremblay <32100830+thecanadianroot@users.noreply.github.com>
2023-01-20 13:35:08 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,85 @@
+# Open pull-request action
+
+This action simply opens a pull-request with the specified inputs.
+
+## Usage
+
+```yaml
+- name: Open pull-request
+  uses: 'thecanadianroot/open-pull-request-action@main'
+  with:
+    # The GitHub access token. Uses 'github.token' by default. Type: string
+    token:  
+    
+    # The base branch for the pull-request. Required. Type: string
+    base: 
+    
+    # The SHA from which the base branch should be created if this input is used. Optional, but the base branch must already exist. Type: string
+    base-sha:
+    
+    # The head branch for the pull-request. Required. Type: string
+    head: 
+    
+    # The SHA from which the head branch should be created if this input is used. Optional, but the head branch must already exist. Type: string
+    head-sha:
+    
+    # The pull-request title. Required. Type: string
+    title: 
+    
+    # The pull-request body. Optional. Type: string
+    body: 
+    
+    # The pull-request assignees. Optional. Type: array
+    assignees:
+
+    # The pull-request labels. Optional. Type: array
+    labels:
+    
+    # The pull-request reviewers. Optional. Type: array
+    reviewers:
+    
+    # The repository owner. Uses current repository owner if not specified. Type: string
+    owner:
+    
+    # The repository name. Uses current repository name if not specified. Type: string
+    repository: 
+```
+
+## Outputs
+
+| Output         | Expected values                                                                                                 | Conditions                                      |
+|----------------|-----------------------------------------------------------------------------------------------------------------|-------------------------------------------------|
+| `base-branch`  | See [Example response](https://docs.github.com/en/rest/git/refs?apiVersion=2022-11-28#create-a-reference)       | Output will exist if a base branch was created  |
+| `head-branch`  | See [Example response](https://docs.github.com/en/rest/git/refs?apiVersion=2022-11-28#create-a-reference)       | Output will exist if an head branch was created |
+| `pull-request` | See [Example response](https://docs.github.com/en/rest/pulls/pulls?apiVersion=2022-11-28#create-a-pull-request) | N/A                                             |
+
+
+## Code in Main
+
+> First, you'll need to have a reasonably modern version of `node` handy. This won't work with versions older than 9, for instance.
+
+Install the dependencies  
+```bash
+$ npm install
+```
+
+Build the typescript and package it for distribution
+```bash
+$ npm run build && npm run package
+```
+
+Run the tests :heavy_check_mark:  
+```bash
+$ npm test
+
+ PASS  ./index.test.js
+  ✓ throws invalid number (3ms)
+  ✓ wait 500 ms (504ms)
+  ✓ test runs (95ms)
+
+...
+```
+
+## Usage:
+
+After testing you can [create a v1 tag](https://github.com/actions/toolkit/blob/master/docs/action-versioning.md) to reference the stable and latest V1 action
---
+++ src/main.ts
@@ -0,0 +1,94 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+
+async function run(): Promise<void> {
+    try {
+        const token: string | undefined = core.getInput('token', {required: true});
+        const base: string | undefined = core.getInput('base', {required: true});
+        const head: string | undefined = core.getInput('head', {required: true});
+        const title: string | undefined = core.getInput('title', {required: true});
+        const body: string | undefined = core.getInput('body', {required: false}) || process.env.BODY;
+        const baseSHA: string | undefined = core.getInput('base-sha', {required: false}) || process.env.BASE_SHA;
+        const headSHA: string | undefined = core.getInput('head-sha', {required: false}) || process.env.HEAD_SHA;
+        const assignees: string[] = core.getMultilineInput('assignees', {required: false}) || process.env.ASSIGNEES;
+        const labels: string[] = core.getMultilineInput('labels', {required: false}) || process.env.LABELS;
+        const reviewers: string[] = core.getMultilineInput('reviewers', {required: false}) || process.env.REVIEWERS;
+        const owner: string = core.getInput('owner') || process.env.OWNER || github.context.repo.owner;
+        const repo: string = core.getInput('repository') || process.env.REPOSITORY || github.context.repo.repo;
+
+        if (!token || !base || !head || !title) {
+            core.setFailed(`'token', 'base', 'head' and 'title' inputs are required!`);
+            return;
+        }
+
+        const octokit = github.getOctokit(token);
+
+        async function createBranch(name: string, sha: string | undefined, ref: string) {
+            if (sha) {
+                const branch = await octokit.rest.git.createRef({
+                    owner,
+                    repo,
+                    ref: `refs/heads/${ref}`,
+                    sha
+                }).catch((reason) => {
+                    core.setFailed(`Couldn't create ${name} branch: ${reason}`);
+                    process.exit(1);
+                });
+                core.info(`Created ${name} branch '${ref}' from SHA '${sha}': https://github.com/${owner}/${repo}/tree/${ref}`)
+                core.setOutput(`${name}-branch`, branch.data);
+            }
+        }
+
+        await createBranch('base', baseSHA, base);
+        await createBranch('head', headSHA, head);
+
+        // Open pull-request from HEAD to BASE
+        const pr = await octokit.rest.pulls.create({
+            owner,
+            repo,
+            title,
+            body,
+            head,
+            base
+        }).catch((reason) => {
+            core.setFailed(`Couldn't open pull-request: ${reason}`);
+            process.exit(1);
+        });
+        core.info(`Opened pull-request #${pr.data.number}: https://github.com/${owner}/${repo}/pulls/${pr.data.number}`)
+        core.setOutput('pull-request', pr.data);
+
+        // Add assignees to pull-request if any
+        if (assignees) {
+            await octokit.rest.issues.addAssignees({
+                owner,
+                repo,
+                issue_number: pr.data.number,
+                assignees
+            }).catch((reason) => core.error(`Couldn't add assignees to pull-request #${pr.data.number}: ${reason}`));
+        }
+
+        // Add labels to pull-request if any.
+        if (labels){
+            await octokit.rest.issues.addLabels({
+                owner,
+                repo,
+                issue_number: pr.data.number,
+                labels
+            })
+        }
+
+        // Add reviewers to pull-request if any.
+        if (reviewers) {
+            await octokit.rest.pulls.requestReviewers({
+                owner,
+                repo,
+                pull_number: pr.data.number,
+                reviewers
+            }).catch((reason) => core.error(`Couldn't add reviewers to pull-request #${pr.data.number}: ${reason}`));
+        }
+    } catch (error) {
+        if (error instanceof Error) core.setFailed(error.message)
+    }
+}
+
+run()
