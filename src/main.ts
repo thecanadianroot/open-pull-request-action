f214dde3816b192d2704fad790bf45ed2743d9d2
thecanadianroot <tremblay.9.0.0.0@gmail.com>
2023-01-20 14:40:27 -0500

    feat: open-pull-request action

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,71 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    try {
+        const token: string | undefined = core.getInput('token', {required: false}) || process.env.TOKEN;
+        const base: string | undefined = core.getInput('base', {required: false}) || process.env.BASE;
+        const head: string | undefined = core.getInput('head', {required: false}) || process.env.HEAD;
+        const sha: string | undefined = core.getInput('sha', {required: false}) || process.env.SHA;
+        const title: string | undefined = core.getInput('title', {required: false}) || process.env.TITLE;
+        const body: string | undefined = core.getInput('body', {required: false}) || process.env.BODY;
+        const assignees: string[] = core.getMultilineInput('assignees', {required: false}) || process.env.ASSIGNEES;
+        const reviewers: string[] = core.getMultilineInput('reviewers', {required: false}) || process.env.REVIEWERS;
+        const owner: string = core.getInput('owner') || process.env.OWNER || github.context.repo.owner;
+        const repo: string = core.getInput('repository') || process.env.REPOSITORY || github.context.repo.repo;
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+        if (!token || !base || !head || !title) {
+            core.setFailed(`'token', 'base', 'head' and 'title' inputs are required!`);
+            core.info(`'TOKEN', 'BASE', 'HEAD' and 'TITLE' environment variables can also be used.`);
+            return;
+        }
 
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
-  }
+        const octokit = github.getOctokit(token);
+
+        // Create HEAD branch from SHA if any
+        if (sha) {
+            const branch = await octokit.rest.git.createRef({
+                owner,
+                repo,
+                ref: `refs/heads/${head}`,
+                sha
+            });
+            core.setOutput('branch', branch.data.ref);
+        }
+
+        // Open pull-request from HEAD to BASE
+        const pr = await octokit.rest.pulls.create({
+            owner,
+            repo,
+            title,
+            body,
+            head,
+            base
+        });
+        core.setOutput('pull-request', pr.data.number);
+
+        // Add assignees to pull-request if any
+        if (assignees) {
+            await octokit.rest.issues.addAssignees({
+                owner,
+                repo,
+                issue_number: pr.data.number,
+                assignees
+            });
+        }
+
+        // Add reviewers to pull-request if any.
+        if (reviewers) {
+            await octokit.rest.pulls.requestReviewers({
+                owner,
+                repo,
+                pull_number: pr.data.number,
+                reviewers
+            });
+        }
+    } catch (error) {
+        if (error instanceof Error) core.setFailed(error.message)
+    }
 }
 
 run()
